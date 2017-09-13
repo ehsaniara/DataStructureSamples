613da6af7bf3a8ed5bbfd7d8d2a40afd33953bad
ehsaniara <ehsaniara@gmail.com>
2017-09-13 14:36:09 -0700

    Factorial Class Adding

--- src/com/ehsaniara/dataStructureSample/SingleLinkedList/LinkedList.java
+++ src/com/ehsaniara/dataStructureSample/SingleLinkedList/LinkedList.java
@@ -7,54 +7,37 @@ package com.ehsaniara.dataStructureSample.SingleLinkedList;
 public class LinkedList {
 
     Node head;
-    private int listCount;
 
-    public LinkedList() {
-        head = new Node(null);
-        listCount = 0;
-    }
-
-    public void add(Object data) {
-        //specified element to the end of this list.
+    //insert at the head
+    public void push(Object data) {
         Node temp = new Node(data);
-        Node current = head;
-        // starting at the head node, crawl to the end of the list
-        while (current.next != null) {
-            current = current.next;
-        }
-        // the last node's "next" reference set to our new node
-        current.next = temp;
-        listCount++;
+        temp.next = head;
+        head = temp;
     }
 
-    public void add(Object data, int index) {
+    public void insertAfter(Node node, Object data) {
+        if (node == null) {
+            System.out.println("node cannot be null");
+            return;
+        }
         Node temp = new Node(data);
-        Node current = head;
-        // crawl to the requested index or the last element in the list,
-        // whichever comes first
-        for (int i = 1; i < index && current.next != null; i++) {
-            current = current.next;
-        }
-        // set the new node's next-node reference to this node's next-node reference
-        temp.next = current.next;
-        // now set this node's next-node reference to the new node
-        current.next = temp;
-        listCount++;// increment the number of elements variable
+        temp.next = node.next;
+
+        //make next of prev_node as new_node
+        node.next = temp;
     }
 
-    public Object get(int index) {
-        // index must be 1 or higher
-        if (index <= 0)
-            return null;
-
-        Node current = head.next;
-        for (int i = 1; i < index; i++) {
-            if (current.next == null)
-                return null;
-
-            current = current.next;
+    public void append(Object data) {
+        Node temp = new Node(data);
+        if (head == null) {
+            push(data);
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = temp;
         }
-        return current.next;
     }
 
     public void show() {
@@ -69,10 +52,19 @@ public class LinkedList {
     public static void main(String[] args) {
 
         LinkedList linkedList = new LinkedList();
-        linkedList.add(1);
-        linkedList.add(2);
-        linkedList.add(3);
-        linkedList.add(4);
+        linkedList.push(1);
+        linkedList.push(2);
+        linkedList.push(3);
+        linkedList.push(4);
+        linkedList.show();
+        System.out.println();
+        linkedList.append(5);
+        linkedList.show();
+        System.out.println();
+        linkedList.push(6);
+        linkedList.show();
+        System.out.println();
+        linkedList.insertAfter(linkedList.head.next, 7);
         linkedList.show();
     }
 
