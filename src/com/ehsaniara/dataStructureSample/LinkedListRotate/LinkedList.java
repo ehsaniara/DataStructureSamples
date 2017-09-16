a3d712d21f1b705eb64972c3befb553750ed075d
ehsaniara <ehsaniara@gmail.com>
2017-09-16 00:42:39 -0700

    Factorial Class Adding

---
+++ src/com/ehsaniara/dataStructureSample/LinkedListRotate/LinkedList.java
@@ -0,0 +1,101 @@
+package com.ehsaniara.dataStructureSample.LinkedListRotate;
+
+/**
+ * Created by Jason Ara (Jay.Ara) on 9/16/17.
+ * From https://www.exapot.com
+ */
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+public class LinkedList {
+    Node head;
+
+    void rotate(int k) {
+        if (k == 0) return;
+
+        // Let us understand the below code for example k = 4
+        // and list = 10->20->30->40->50->60.
+        Node current = head;
+
+        // current will either point to kth or NULL after this
+        // loop. current will point to node 40 in the above example
+        int count = 1;
+        while (count < k && current != null) {
+            current = current.next;
+            count++;
+        }
+
+        // If current is NULL, k is greater than or equal to count
+        // of nodes in linked list. Don't change the list in this case
+        if (current == null)
+            return;
+
+        // current points to kth node. Store it in a variable.
+        // kthNode points to node 40 in the above example
+        Node kthNode = current;
+
+        // current will point to last node after this loop
+        // current will point to node 60 in the above example
+        while (current.next != null)
+            current = current.next;
+
+        // Change next of last node to previous head
+        // Next of 60 is now changed to node 10
+
+        current.next = head;
+
+        // Change head to (k+1)th node
+        // head is now changed to node 50
+        head = kthNode.next;
+
+        // change next of kth node to null
+        kthNode.next = null;
+
+    }
+
+    void push(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    /* Drier program to test above functions */
+    public static void main(String args[]) {
+        LinkedList llist = new LinkedList();
+
+        // create a list 10->20->30->40->50->60
+        /*
+        for (int i = 60; i >= 10; i -= 10)
+            llist.push(i);
+            */
+        int[] arr = {60, 50, 40, 30, 20, 10};
+        for (int item : arr) {
+            llist.push(item);
+        }
+
+        System.out.println("Given list");
+        llist.printList();
+
+        llist.rotate(4);
+
+        System.out.println("Rotated Linked List");
+        llist.printList();
+    }
+
+}
---
+++ src/com/ehsaniara/dataStructureSample/LinkedListRotate/README.md
@@ -0,0 +1,3 @@
+# Rotate a Linked List
+Given a singly linked list, rotate the linked list counter-clockwise by k nodes. Where k is a given positive integer. For example, if the given linked list is 10->20->30->40->50->60 and k is 4, the list should be modified to 50->60->10->20->30->40. Assume that k is smaller than the count of nodes in linked list.
+
